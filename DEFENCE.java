43d8bdd37f4a7f2ed8f1a8e4740d7fb3745bed25
Yerlan Smith <131413232+yerlanello@users.noreply.github.com>
2024-04-26 08:42:26 +0500

    Update DEFENCE.java

--- DEFENCE.java
+++ DEFENCE.java
@@ -1,10 +1,28 @@
 //we have the string and need to find countity of given letter
-public class DEFENCE {
+import java.util.Scanner;
 
-    @Override
-    public static int Countity_Of_Letter() {
+public class LetterCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String str = scanner.nextLine();
 
+        char target = scanner.next().charAt(0);
 
+        int count = countLetter(str, target, 0);
+
+        System.out.println("The count of '" + target + "' in the string is: " + count);
+
+        scanner.close();
+    }
+
+    public static int countLetter(String str, char target, int index) {
+        if (index == str.length()) {
+            return 0;
+        }
+
+        int count = (str.charAt(index) == target) ? 1 : 0;
+
+        return count + countLetter(str, target, index + 1);
     }
 }
